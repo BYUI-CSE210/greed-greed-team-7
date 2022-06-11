c19acf03873390faff2f9aa3bc1431030c48ff0b
KobeLemon <104539403+KobeLemon@users.noreply.github.com>
2022-06-10 19:04:00 -0600

    Fixed a glitch, added a special item, fixed grammar errors, changed "stone" to "rock"

--- greed/game/directing/director.py
+++ greed/game/directing/director.py
@@ -59,55 +59,73 @@ class Director:
         blue = Color(0, 0, 255)
         red = Color(255, 0, 0)
         white = Color(255, 255, 255)
+        random_color = Color(random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
 
         # Falling Objects
 
         #Creating instances for each one of the rocks and gems
-        stone = RockGem()
+        rock = RockGem()
         green_gem = RockGem()
         light_blue_gem = RockGem()
         blue_gem = RockGem()
         red_gem = RockGem()
 
-        #Setting the valie for the Flying objects it could be a rock or a gem 
-        stone.set_text("0")
+        # Special rocks and gems
+        special_number = RockGem.get_special_item(self)
+        special_gem = RockGem()
+        special_rock = RockGem()
+        if special_number == True:
+            special_rock.set_text("0")
+            special_rock.set_velocity(Point(0,5))
+            special_rock.set_color(random_color)
+            special_rock.set_font_size(50)
+            cast.add_actor("things", special_rock)
+            special_rock.set_position(Point(random.randint(0, 900),1))
+            special_gem.set_text("$")
+            special_gem.set_velocity(Point(0,5))
+            special_gem.set_color(random_color)
+            special_gem.set_font_size(50)
+            cast.add_actor("things", special_gem)
+            special_gem.set_position(Point(random.randint(0, 900),1))
+
+        #Setting the value for the Flying objects it could be a rock or a gem 
+        rock.set_text("0")
         green_gem.set_text("*")
         light_blue_gem.set_text("*")
         blue_gem.set_text("*")
         red_gem.set_text("*")
-
+        
         # Setting the rate (velocity) for the falling objects - Rocks and gems
-        stone.set_velocity(Point(0,5))        
+        rock.set_velocity(Point(0,5))        
         green_gem.set_velocity(Point(0,5))
         blue_gem.set_velocity(Point(0,5))
         red_gem.set_velocity(Point(0,5))
-
-        #Creatin a random position for each Flying object Rock and Gems
-        stone.set_position(Point(random.randint(0, 900),50))
+        
+        # Create a random position for each Flying object Rock and Gems
+        rock.set_position(Point(random.randint(0, 900),50))
         green_gem.set_position(Point(random.randint(0, 900),1))
         light_blue_gem.set_position(Point(random.randint(0, 900),1))
         blue_gem.set_position(Point(random.randint(0, 900),1))
         red_gem.set_position(Point(random.randint(0, 900),1))
 
-
         #assigning the color to each rock and Gem
-        stone.set_color(white)
+        rock.set_color(white)
         green_gem.set_color(green)
         blue_gem.set_color(blue)
         red_gem.set_color(red)
 
         #Using the set_font_size and a value for rocks and gems
-        stone.set_font_size(25)
+        rock.set_font_size(25)
         green_gem.set_font_size(25)
         blue_gem.set_font_size(25)
         red_gem.set_font_size(25)
 
-        # establishes gems and stones
-        cast.add_actor("things", stone)
+        # establishes gems and rocks
+        cast.add_actor("things", rock)
         cast.add_actor("things", green_gem)
         cast.add_actor("things", light_blue_gem)
         cast.add_actor("things", blue_gem)
-        cast.add_actor("things",red_gem)
+        cast.add_actor("things", red_gem)
 
         # Move everything        
         score = cast.get_first_actor("score")
@@ -118,31 +136,52 @@ class Director:
         y_max = self._video_service.get_height()
         player.move_next(y_max, y_max)
 
-
-        #Logic of the points sistem. 
+        # Logic of the points system. 
         for actor in cast.get_actors("things"):
             actor.move_next(x_max, y_max)
-
+            # Regular Gem
             if actor.get_text() == "*":
                 actor_x = actor.get_position().get_x()
                 actor_y = actor.get_position().get_y()
 
                 if ((x_player - 10 < actor_x < x_player + 10) and (y_player - 10 < actor_y < y_player + 10)):
                     score.add_points(100)
-                if actor_y > y_max - 30 or((x_player - 10 < actor_x < x_player + 10) and (y_player - 10 < actor_y < y_player + 10)):
+                    score.set_text("Good job! You found a Gem!")
+                if actor_y > y_max - 30 or ((x_player - 10 < actor_x < x_player + 10) and (y_player - 10 < actor_y < y_player + 10)):
                     cast.remove_actor("things", actor)
-
-            elif actor.get_text() == "o":
+            # Regular Rock
+            elif actor.get_text() == "0":
                 actor_x = actor.get_position().get_x()
                 actor_y = actor.get_position().get_y()
 
-                if ((x_player - 10 < actor_x < x_player + 10) and (y_player - 10 < actor_y <y_player + 10)):
+                if ((x_player - 10 < actor_x < x_player + 10) and (y_player - 10 < actor_y < y_player + 10)):
                     score.add_points(-100)
+                    score.set_text("Bad luck! You found a rock!")
                 if actor_y > y_max - 30 or ((x_player - 10 < actor_x < x_player + 10) and (y_player - 10 < actor_y < y_player + 10)):
                     cast.remove_actor("things", actor)
 
+            # Special Gem
+            elif actor.get_text() == "$":
+                actor_x = actor.get_position().get_x()
+                actor_y = actor.get_position().get_y()
 
-        
+                if ((x_player - 40 < actor_x < x_player + 50) and (y_player - 40 < actor_y < y_player + 50)):
+                    score.add_points(1000)
+                    score.set_text("Good job! You found a Giant Gem!")
+                if actor_y > y_max - 40 or ((x_player - 40 < actor_x < x_player + 50) and (y_player - 40 < actor_y < y_player + 40)):
+                    cast.remove_actor("things", actor)
+                
+            # Special rock
+            elif actor.get_text() == "%":
+                actor_x = actor.get_position().get_x()
+                actor_y = actor.get_position().get_y()  
+
+                if ((x_player - 40 < actor_x < x_player + 50) and (y_player - 40 < actor_y < y_player + 50)):
+                    score.add_points(-1000)
+                    score.set_text("Bad luck! You found a Giant Rock!")
+                if actor_y > y_max - 40 or ((x_player - 40 < actor_x < x_player + 50) and (y_player - 40 < actor_y < y_player + 40)):
+                    cast.remove_actor("things", actor)
+
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
         
